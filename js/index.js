6baebee260e944b0b044d610ff933f95033558f8
sidemt <25644062+sidemt@users.noreply.github.com>
2018-10-05 06:00:50 +0900

    First commit

---
+++ js/index.js
@@ -0,0 +1,65 @@
+var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self, call) {if (!self) {throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call && (typeof call === "object" || typeof call === "function") ? call : self;}function _inherits(subClass, superClass) {if (typeof superClass !== "function" && superClass !== null) {throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);}subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;}var nums = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine' };var
+
+
+Calculator = function (_React$Component) {_inherits(Calculator, _React$Component);
+  function Calculator(props) {_classCallCheck(this, Calculator);var _this = _possibleConstructorReturn(this, (Calculator.__proto__ || Object.getPrototypeOf(Calculator)).call(this,
+    props));
+    _this.state = {
+      num: 0 };return _this;
+
+  }_createClass(Calculator, [{ key: 'render', value: function render()
+    {
+      var numKeys = [];
+      for (var i = 0; i <= 9; i++) {
+        numKeys.push(
+        React.createElement(RenderNumKey, { keyName: nums[i], keyNum: i }));
+
+      }
+      return (
+        React.createElement('div', null, 'Calculator',
+
+          React.createElement('div', null, this.state.num),
+          React.createElement(RenderKey, { keyName: 'equals', keyValue: '=' }),
+          numKeys,
+          React.createElement(RenderKey, { keyName: 'add', keyValue: '+' }),
+          React.createElement(RenderKey, { keyName: 'subtract', keyValue: '-' }),
+          React.createElement(RenderKey, { keyName: 'multiply', keyValue: '*' }),
+          React.createElement(RenderKey, { keyName: 'divide', keyValue: '/' })));
+
+
+    } }]);return Calculator;}(React.Component);
+;var
+
+RenderKey = function (_React$Component2) {_inherits(RenderKey, _React$Component2);
+  function RenderKey(props) {_classCallCheck(this, RenderKey);return _possibleConstructorReturn(this, (RenderKey.__proto__ || Object.getPrototypeOf(RenderKey)).call(this,
+    props));
+  }_createClass(RenderKey, [{ key: 'render', value: function render()
+    {
+      return (
+        React.createElement('button', { id: this.props.keyName }, this.props.keyValue));
+
+    } }]);return RenderKey;}(React.Component);var
+
+
+RenderNumKey = function (_React$Component3) {_inherits(RenderNumKey, _React$Component3);
+  function RenderNumKey(props) {_classCallCheck(this, RenderNumKey);return _possibleConstructorReturn(this, (RenderNumKey.__proto__ || Object.getPrototypeOf(RenderNumKey)).call(this,
+    props));
+  }_createClass(RenderNumKey, [{ key: 'render', value: function render()
+    {
+      return (
+        React.createElement('button', { id: this.props.keyName }, this.props.keyNum));
+
+    } }]);return RenderNumKey;}(React.Component);
+
+
+ReactDOM.render(React.createElement(Calculator, null), document.getElementById('calculator'));
