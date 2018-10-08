8486d700a56227fe70844fe7917dd166082b888b
sidemt <25644062+sidemt@users.noreply.github.com>
2018-10-08 16:33:50 +0900

    Handle the case when operator keys are pressed consecutively, make all tests pass

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self, call) {if (!self) {throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call && (typeof call === "object" || typeof call === "function") ? call : self;}function _inherits(subClass, superClass) {if (typeof superClass !== "function" && superClass !== null) {throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);}subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;}var nums = {
+var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();function _toConsumableArray(arr) {if (Array.isArray(arr)) {for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {arr2[i] = arr[i];}return arr2;} else {return Array.from(arr);}}function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self, call) {if (!self) {throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call && (typeof call === "object" || typeof call === "function") ? call : self;}function _inherits(subClass, superClass) {if (typeof superClass !== "function" && superClass !== null) {throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);}subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;}var nums = {
   0: 'zero',
   1: 'one',
   2: 'two',
@@ -8,17 +8,19 @@ var _createClass = function () {function defineProperties(target, props) {for (v
   6: 'six',
   7: 'seven',
   8: 'eight',
-  9: 'nine' };var
+  9: 'nine' };
 
+
+var operators = ['/', '*', '+', '-'];var
 
 Calculator = function (_React$Component) {_inherits(Calculator, _React$Component);
   function Calculator(props) {_classCallCheck(this, Calculator);var _this = _possibleConstructorReturn(this, (Calculator.__proto__ || Object.getPrototypeOf(Calculator)).call(this,
     props));
     _this.state = {
-      input: '',
+      input: '0',
       calculations: [] };
 
-    _this.handleClick = _this.handleClick.bind(_this);
+    _this.handleNumber = _this.handleNumber.bind(_this);
     _this.handleOperator = _this.handleOperator.bind(_this);
     _this.clear = _this.clear.bind(_this);
     _this.dot = _this.dot.bind(_this);
@@ -33,9 +35,17 @@ Calculator = function (_React$Component) {_inherits(Calculator, _React$Component
 
     } }, { key: 'dot', value: function dot()
     {
-      if (this.state.input.indexOf(".") == -1) {
+      if (this.state.input == '') {
+        this.setState({
+          input: "0." });
+
+      } else if (this.state.input.indexOf(".") == -1) {
         this.setState({
           input: this.state.input + "." });
+
+      } else {
+        this.setState({
+          input: this.state.input });
 
       };
     } }, { key: 'zero', value: function zero()
@@ -45,18 +55,34 @@ Calculator = function (_React$Component) {_inherits(Calculator, _React$Component
           input: this.state.input + '0' });
 
       };
-    } }, { key: 'handleClick', value: function handleClick(
+    } }, { key: 'handleNumber', value: function handleNumber(
     event) {
-      this.setState({
-        input: this.state.input + event.target.innerText });
+      if (this.state.input == '0') {
+        this.setState({
+          input: event.target.innerText });
 
+      } else {
+        this.setState({
+          input: this.state.input.toString() + event.target.innerText });
+
+      }
     } }, { key: 'handleOperator', value: function handleOperator(
     event) {
       var currentInput = this.state.input;
-      this.setState({
-        input: '',
-        calculations: this.state.calculations.concat(currentInput, event.target.innerText) });
+      var newCalculations = [].concat(_toConsumableArray(this.state.calculations));
+      if (currentInput == '') {
+        newCalculations.splice(this.state.calculations.length - 1, 1, event.target.innerText);
+        this.setState({
+          input: '',
+          calculations: newCalculations });
 
+        console.log(this.state.calculations);
+      } else {
+        this.setState({
+          input: '',
+          calculations: newCalculations.concat(currentInput, event.target.innerText) });
+
+      }
     } }, { key: 'calculate', value: function calculate(
     str) {
       console.log(str);
@@ -64,21 +90,22 @@ Calculator = function (_React$Component) {_inherits(Calculator, _React$Component
         var f = new Function('return ' + str + ';');
         return f();
       } else {
-        return 0;
+        return '0';
       }
     } }, { key: 'equals', value: function equals(
     event) {
       var currentInput = this.state.input;
+      var currentCalculations = this.state.calculations;
       this.setState({
-        input: '0',
-        calculations: this.calculate(this.state.calculations.concat(currentInput).join('')) });
+        input: this.calculate(currentCalculations.concat(currentInput).join('')),
+        calculations: [] });
 
     } }, { key: 'render', value: function render()
     {
       var numKeys = [];
       for (var i = 0; i <= 9; i++) {
         numKeys.push(
-        React.createElement(RenderNumKey, { keyName: nums[i], keyNum: i, onClick: this.handleClick }));
+        React.createElement(RenderNumKey, { keyName: nums[i], keyNum: i, onClick: this.handleNumber }));
 
       }
       return (
@@ -87,7 +114,7 @@ Calculator = function (_React$Component) {_inherits(Calculator, _React$Component
           React.createElement('div', null, 'input: ', this.state.input),
           React.createElement('div', null, 'calculations: ', this.state.calculations),
           React.createElement('div', { className: 'row' },
-            React.createElement('div', { id: 'display' }, this.state.input)),
+            React.createElement('div', { id: 'display' }, this.state.calculations, this.state.input)),
 
 
           React.createElement('div', { className: 'row' },
